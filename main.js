dragonwocky/obsidian-tray

commit 5cef2b0ed7f67bbba43456148020df904bbe3b87

fix #22: preserve maximised windows after minimisation

main.js
@@ -41,47 +41,41 @@ const obsidian = require("obsidian"),
   { app, Tray, Menu, nativeImage } = require("electron").remote,
   { getCurrentWindow, globalShortcut } = require("electron").remote;
 
-const childWindows = new Set(),
-  observeChildWindows = () => {
-    getCurrentWindow().webContents.on("did-create-window", (win) => {
-      childWindows.add(win);
-      win.on("close", () => childWindows.delete(win));
+const vaultWindows = new Set(),
+  maximizedWindows = new Set(),
+  getWindows = () => [...vaultWindows],
+  observeWindows = () => {
+    const onWindowCreation = (win) => {
+      vaultWindows.add(win);
       win.setSkipTaskbar(plugin.settings.hideTaskbarIcon);
-    });
+      win.on("close", () => vaultWindows.delete(win));
+      // preserve maximised windows after minimisation
+      if (win.isMaximized()) maximizedWindows.add(win);
+      win.on("maximize", () => maximizedWindows.add(win));
+      win.on("unmaximize", () => maximizedWindows.delete(win));
+    };
+    onWindowCreation(getCurrentWindow());
+    getCurrentWindow().webContents.on("did-create-window", onWindowCreation);
   },
-  getAllWindows = () => [...childWindows, getCurrentWindow()],
   showWindows = () => {
     log(LOG_SHOWING_WINDOWS);
-    getAllWindows().forEach((win) => win.show());
+    getWindows().forEach((win) => {
+      maximizedWindows.has(win) ? win.maximize() : win.show();
+    });
   },
   hideWindows = () => {
     log(LOG_HIDING_WINDOWS);
-    getAllWindows().forEach((win) => [
+    getWindows().forEach((win) => [
       win.isFocused() && win.blur(),
       plugin.settings.runInBackground ? win.hide() : win.minimize(),
     ]);
   },
   toggleWindows = (checkForFocus = true) => {
-    const openWindows = getAllWindows().some((win) => {
+    const openWindows = getWindows().some((win) => {
       return (!checkForFocus || win.isFocused()) && win.isVisible();
     });
     if (openWindows) hideWindows();
     else showWindows();
-  };
-
-let isMaximized = false;
-const onMinimize = () => {
-    if (isMaximized) getCurrentWindow().maximize();
-    hideWindows();
-  }, interceptWindowMinimize = () => {
-    isMaximized = getCurrentWindow().isMaximized();
-    getCurrentWindow().on("maximize", () => {
-      isMaximized = true;
-    });
-    getCurrentWindow().on("unmaximize", () => {
-      isMaximized = false;
-    });
-    getCurrentWindow().on("minimize", onMinimize);
   };
 
 const onWindowClose = (event) => event.preventDefault(),
@@ -107,7 +101,7 @@ const onWindowClose = (event) => event.preventDefault(),
   };
 
 const setHideTaskbarIcon = () => {
-    getAllWindows().forEach((win) => {
+    getWindows().forEach((win) => {
       win.setSkipTaskbar(plugin.settings.hideTaskbarIcon);
     });
   },
@@ -127,7 +121,7 @@ const setHideTaskbarIcon = () => {
     unregisterHotkeys();
     allowWindowClose();
     destroyTray();
-    getAllWindows().forEach((win) => win.destroy());
+    getWindows().forEach((win) => win.destroy());
   };
 
 const addQuickNote = () => {
@@ -408,11 +402,8 @@ class TrayPlugin extends obsidian.Plugin {
     registerHotkeys();
     setHideTaskbarIcon();
     setLaunchOnStartup();
-    observeChildWindows();
-    if (settings.runInBackground) {
-      interceptWindowMinimize();
-      interceptWindowClose();
-    }
+    observeWindows();
+    if (settings.runInBackground) interceptWindowClose();
     if (settings.hideOnLaunch) {
       this.registerEvent(this.app.workspace.onLayoutReady(hideWindows));
     }

commit b62d8862ab9564662b8112cedd0ed453ebb6878a

theoretically fix #28: quit app on macos if no windows remain
fix: keep track of current window even after 'close' for toggling back to focus with hotkey if run in background enabled (bug introduced in 5cef2b0ed7f67bbba43456148020df904bbe3b87)

main.js
@@ -38,7 +38,8 @@ const LOG_PREFIX = "obsidian-tray",
 
 let tray, plugin;
 const obsidian = require("obsidian"),
-  { app, Tray, Menu, nativeImage } = require("electron").remote,
+  { app, Tray, Menu } = require("electron").remote,
+  { nativeImage, BrowserWindow } = require("electron").remote,
   { getCurrentWindow, globalShortcut } = require("electron").remote;
 
 const vaultWindows = new Set(),
@@ -48,7 +49,9 @@ const vaultWindows = new Set(),
     const onWindowCreation = (win) => {
       vaultWindows.add(win);
       win.setSkipTaskbar(plugin.settings.hideTaskbarIcon);
-      win.on("close", () => vaultWindows.delete(win));
+      win.on("close", () => {
+        if (win !== getCurrentWindow()) vaultWindows.delete(win);
+      });
       // preserve maximised windows after minimisation
       if (win.isMaximized()) maximizedWindows.add(win);
       win.on("maximize", () => maximizedWindows.add(win));
@@ -125,7 +128,9 @@ const setLaunchOnStartup = () => {
     unregisterHotkeys();
     allowWindowClose();
     destroyTray();
-    getWindows().forEach((win) => win.destroy());
+    for (const win of getWindows()) win.destroy();
+    // force app quit if no windows remain on macos
+    if (!BrowserWindow.getAllWindows().length) app.quit();
   };
 
 const addQuickNote = () => {
@@ -240,8 +245,8 @@ const OPTIONS = [
     default: false,
     onChange() {
       setLaunchOnStartup();
-      const runInBackground = plugin.settings.runInBackground;
-      if (!runInBackground) showWindows();
+      if (plugin.settings.runInBackground) interceptWindowClose();
+      else [allowWindowClose(), showWindows()];
     },
   },
   {